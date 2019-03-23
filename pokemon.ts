2911b804e98b427c1b58b0f11da54b7096674b86
shih_jun_teh <shih_jun_teh@dell.com>
2019-03-23 16:30:08 +0800

    pokemon Implement ATK and DEF values

--- pokemon.ts
+++ pokemon.ts
@@ -1,23 +1,54 @@
 import { question } from "readline-sync";
 
-const myPokemon = "Pika"
-const opponent = "Magikarp"
-//const mySkill = "Lightning"
-//const oppSkill = "Shadow Ball"
-let myHp = 100
-let oppHP = 100
-//let myskillDmg = 45
-//let oppskillDmg = 30
 let isMyturn = true
 
-// 1. You have encounter your opponent Mr Mine.
-console.log("You have encounter your opponent " + opponent)
-console.log(opponent + " has " + oppHP + " HP")
-// 2. You send in Gengar.
-console.log("You send in " + myPokemon)
-console.log(myPokemon + " has " + myHp + " HP")
+//Pokemon types
+function displaypokemonOption(array) {
+  let i = 0
+  while (i < 2) {
+    console.log(i + 1 + '. ' + array[i].name + " with " + array[i].hp + " HP. Atk: " + array[i].atk + ", Def: " + array[i].def)
+    i = i + 1
+  }
+}
 
-function displayOption(array) {
+const pokemonOptions = [
+  {
+    name: 'Pikachu',
+    hp: 150,
+    atk: 20,
+    def: 20,
+  },
+  {
+    name: 'Magikarp',
+    hp: 100,
+    atk: 25,
+    def: 25,
+  }
+]
+
+// 1. You have encounter your opponent.
+console.log("You have encounter your opponent.")
+console.log("Pokemon options:")
+displaypokemonOption(pokemonOptions)
+let oppPokemon = parseInt(question("What Pokemon your opponent want to use?\n"))
+oppPokemon = oppPokemon - 1
+console.log("Your opponent send in " + pokemonOptions[oppPokemon].name)
+console.log(pokemonOptions[oppPokemon].name + " stats:")
+console.log("HP: " + pokemonOptions[oppPokemon].hp + "\nATK: " + pokemonOptions[oppPokemon].atk + "\nDEF: " + pokemonOptions[oppPokemon].def)
+
+// 2. You send in your pokemon.
+console.log("==========================")
+console.log("Pokemon options:")
+displaypokemonOption(pokemonOptions)
+let myPokemon = parseInt(question("What Pokemon you want to use?\n"))
+myPokemon = myPokemon - 1
+console.log("You send in " + pokemonOptions[myPokemon].name)
+console.log(pokemonOptions[myPokemon].name + " stats:")
+console.log("HP: " + pokemonOptions[myPokemon].hp + "\nATK: " + pokemonOptions[myPokemon].atk + "\nDEF: " + pokemonOptions[myPokemon].def)
+
+
+//move types
+function displaymoveOption(array) {
   let i = 0
   while (i < 4) {
     console.log(i + 1 + '. ' + array[i].name + " with " + array[i].type + " type.")
@@ -49,41 +80,40 @@ const movesOptions = [
 ]
 
 
-// 3. You can choose one of Gengar's skills
-while (myHp > 0 && oppHP > 0) {
-  let attackerName = isMyturn ? myPokemon : opponent
-  let defenderName = isMyturn ? opponent : myPokemon
+//3. You can choose one of pokemon's skills
+while (pokemonOptions[myPokemon].hp > 0 && pokemonOptions[oppPokemon].hp > 0) {
+  let attackerName = isMyturn ? pokemonOptions[myPokemon].name : pokemonOptions[oppPokemon].name
+  let defenderName = isMyturn ? pokemonOptions[oppPokemon].name : pokemonOptions[myPokemon].name
   //let attackerSkill = isMyturn ? mySkill : oppSkill
 
-  //random number between 10-50
-  //const attackerDmg = Math.floor(Math.random() * 40 + 10)
-
-
+  //   //random number between 10-50
+  //   //const attackerDmg = Math.floor(Math.random() * 40 + 10)
 
   console.log("==========================")
   console.log("It's " + attackerName + "'s turn!")
-  displayOption(movesOptions)
+  console.log("Available move:")
+  displaymoveOption(movesOptions)
   let ansMove = parseInt(question("What move " + attackerName + " want to use?\n"))
   ansMove = ansMove - 1
   console.log(attackerName + " use " + movesOptions[ansMove].name)
   console.log(movesOptions[ansMove].name + " hits for " + movesOptions[ansMove].damage)
 
   if (isMyturn) {
-    oppHP = oppHP - movesOptions[ansMove].damage
-    console.log(defenderName + "'s health is reduce to " + oppHP)
+    pokemonOptions[oppPokemon].hp = pokemonOptions[oppPokemon].hp - (movesOptions[ansMove].damage + pokemonOptions[myPokemon].atk - pokemonOptions[oppPokemon].def)
+    console.log(defenderName + "'s health is reduce to " + pokemonOptions[oppPokemon].hp)
   } else {
-    myHp = myHp - movesOptions[ansMove].damage
-    console.log(defenderName + "'s health is reduce to " + myHp)
+    pokemonOptions[myPokemon].hp = pokemonOptions[myPokemon].hp - (movesOptions[ansMove].damage + pokemonOptions[oppPokemon].atk - pokemonOptions[myPokemon].def)
+    console.log(defenderName + "'s health is reduce to " + pokemonOptions[myPokemon].hp)
   }
   isMyturn = !isMyturn
 }
 
-if (oppHP <= 0) {
-  console.log(opponent + " get Critical Hit.")
-  console.log(opponent + " fainted because health reduced to " + oppHP)
+if (pokemonOptions[oppPokemon].hp <= 0) {
+  console.log(pokemonOptions[oppPokemon].name + " get Critical Hit.")
+  console.log(pokemonOptions[oppPokemon].name + " fainted because health reduced to " + pokemonOptions[oppPokemon].hp)
   console.log("Game Over")
-} else if (myHp <= 0) {
-  console.log(myPokemon + " get Critical Hit.")
-  console.log(myPokemon + " fainted because health reduced to " + oppHP)
+} else if (pokemonOptions[myPokemon].hp <= 0) {
+  console.log(pokemonOptions[myPokemon].name + " get Critical Hit.")
+  console.log(pokemonOptions[myPokemon].name + " fainted because health reduced to " + pokemonOptions[myPokemon].hp)
   console.log("Game Over")
 }
